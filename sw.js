8a8b7ae7d509e6efeb0240ec69cd0debcccc0ca0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-15 10:16:20 +0000

    Deploy JSConfBp/jsconfbp-2019-booklet to github.com/JSConfBp/jsconfbp-2019-booklet.git:gh-pages

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-08002527df546f3fb7e7.js"
+    "url": "webpack-runtime-95b4e26e459d99b4dbc9.js"
   },
   {
     "url": "commons-4f71393d794b135e7ba7.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "558c5316f57d2e254d111994f5ab7438"
+    "revision": "005d59334c2701f061e7f3d0da9efe9c"
   },
   {
     "url": "google-fonts/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2",
@@ -68,7 +68,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "d5bc82d55f1d667a74cb652ec88cdb89"
+    "revision": "479fec0b2a41aa7b27054dd3d7923804"
   },
   {
     "url": "manifest.webmanifest",
